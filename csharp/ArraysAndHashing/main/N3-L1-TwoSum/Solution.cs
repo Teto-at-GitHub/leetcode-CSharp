4359ae6e73aaee0a4240d4e968eae05c3cf209ec
TaetoTum <stefanocolucci1988@gmail.com>
2022-05-05 22:37:59 +0200

    FEAT : adds O(n) solution to TwoSum

--- csharp/ArraysAndHashing/main/N3-L1-TwoSum/Solution.cs
+++ csharp/ArraysAndHashing/main/N3-L1-TwoSum/Solution.cs
@@ -3,17 +3,20 @@ namespace ArrayAndHashing.Main.TwoSum
   public class Solution
   {
 
-public int[] TwoSum(int[] nums, int target)
+    // time complexity : O(n^2)
+    // space complexity : O(2n)
+    // where n = nums.Length
+    public int[] TwoSumN2(int[] nums, int target)
     {
-    int[] retval = new int[2];
-      r (int i = 0; i < nums.Length; i++)
+      int[] retval = new int[2];
+      for (int i = 0; i < nums.Length; i++)
       {
-    for (int j = i + 1; j < nums.Length; j++)
+        for (int j = i + 1; j < nums.Length; j++)
         {
-        if (nums[i] + nums[j] == target)
+          if (nums[i] + nums[j] == target)
           {
-        retval[0] = i;
-            al[1] = j;
+            retval[0] = i;
+            retval[1] = j;
             break;
           }
         }
@@ -21,7 +24,33 @@ public int[] TwoSum(int[] nums, int target)
         if (retval[1] != 0)
           break;
       }
-    return retval;
+      return retval;
     }
-}
+
+    // time complexity : O(n) = only one for loop +
+    //      in a Dictionary retrieval time by key is O(1)
+    // space complexity : O(n) = the size of the dictionary
+    public int[] TwoSum(int[] nums, int target)
+    {
+      int[] retval = new int[2];
+      Dictionary<int, int> map = new();
+      for (int i = 0; i < nums.Length; i++)
+      {
+        int current_elem = nums[i];
+        int diff = target - current_elem;
+        if (map.ContainsKey(diff))
+        {
+          retval[0] = map[diff];
+          retval[1] = i;
+          break;
+        }
+        // you may have repeated elements in nums, see test case 4
+        if (!map.ContainsKey(current_elem))
+          map.Add(current_elem, i);
+      }
+      return retval;
+    }
+
+  }
+
 }
--- csharp/ArraysAndHashing/testCases/N3-L1-TwoSum_Test.cs
+++ csharp/ArraysAndHashing/testCases/N3-L1-TwoSum_Test.cs
@@ -4,48 +4,62 @@ using NUnit.Framework;
 
 namespace testCases
 {
-   public class TwoSum_Test
-   {
-      [Test]
-      public void Test_Example_1()
-      {
-         int[] nums = {2,7,11,15};
-         int target = 9;
+  public class TwoSum_Test
+  {
+    [Test]
+    public void Test_Example_1()
+    {
+      int[] nums = { 2, 7, 11, 15 };
+      int target = 9;
 
-         Solution solution = new Solution();
-         int[] testResult = solution.TwoSum(nums,target);
+      Solution solution = new Solution();
+      int[] testResult = solution.TwoSum(nums, target);
 
-         testResult[0].Should().Be(0);
-         testResult[1].Should().Be(1);
-         
-      }
+      testResult[0].Should().Be(0);
+      testResult[1].Should().Be(1);
 
-      [Test]
-      public void Test_Example_2()
-      {
-         int[] nums = { 3,2,4 };
-         int target = 6;
+    }
 
-         Solution solution = new Solution();
-         int[] testResult = solution.TwoSum(nums, target);
+    [Test]
+    public void Test_Example_2()
+    {
+      int[] nums = { 3, 2, 4 };
+      int target = 6;
 
-         testResult[0].Should().Be(1);
-         testResult[1].Should().Be(2);
-      }
+      Solution solution = new Solution();
+      int[] testResult = solution.TwoSum(nums, target);
 
-       [Test]
-       public void Test_Example_3()
-      { 
-         int[] nums = { 3,3 };
-         int target = 6;
+      testResult[0].Should().Be(1);
+      testResult[1].Should().Be(2);
+    }
 
-         Solution solution = new Solution();
-         int[] testResult = solution.TwoSum(nums, target);
+    [Test]
+    public void Test_Example_3()
+    {
+      int[] nums = { 3, 3 };
+      int target = 6;
 
-         testResult[0].Should().Be(0);
-         testResult[1].Should().Be(1);
+      Solution solution = new Solution();
+      int[] testResult = solution.TwoSum(nums, target);
 
-      }
+      testResult[0].Should().Be(0);
+      testResult[1].Should().Be(1);
+
+    }
+
+    [Test]
+    public void Test_Example_4()
+    {
+      int[] nums = { 1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1 };
+      int target = 11;
+
+      Solution solution = new Solution();
+      int[] testResult = solution.TwoSum(nums, target);
+
+      testResult[0].Should().Be(5);
+      testResult[1].Should().Be(11);
+
+    }
 
   }
 }
