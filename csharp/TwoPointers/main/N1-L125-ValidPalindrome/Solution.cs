ac260d11308124ab6db60007b717c29522ce8cf5
Stefano Colucci <stefano.colucci@ubidata.com>
2022-05-07 19:44:52 +0200

    FEAT : adds an improved solution and a faster one actually using pointers

--- csharp/TwoPointers/main/N1-L125-ValidPalindrome/Solution.cs
+++ csharp/TwoPointers/main/N1-L125-ValidPalindrome/Solution.cs
@@ -6,24 +6,74 @@ namespace TwoPointers.Main.ValidPalindrome
    {
       public bool IsPalindrome(string s)
       {
-         List<char> cleanInput = new();
-         foreach (char item in s)
-         {
-            Regex regex = new Regex("[a-zA-Z0-9]");
-            Match match = regex.Match(item.ToString());
+         //FIRST SUBMISSION
+         // List<char> cleanInput = new();
+         // foreach (char item in s)
+         // {
+         //    Regex regex = new Regex("[a-zA-Z0-9]");
+         //    Match match = regex.Match(item.ToString());
 
-            if (match.Success)
-               cleanInput.Add(item.ToString().ToLower().ElementAt(0));
-         }
+         //    if (match.Success)
+         //       cleanInput.Add(item.ToString().ToLower().ElementAt(0));
+         // }
 
-         int cleanInputLength = cleanInput.Count;
-         for (int i = 0; i < cleanInputLength / 2; i++)
+         // A bit slower
+         // int cleanInputLength = cleanInput.Count;
+         // for (int i = 0; i < cleanInputLength / 2; i++)
+         // {
+         //    if (cleanInput[i] != cleanInput[cleanInputLength - 1 - i])
+         //       return false;
+         // }
+
+         // FINAL submission
+         // I like this solution because :
+         // - it's faster than the previous one : 81 < 586
+         // - it uses built-in fcts to perform the checks :
+         //    - if a char is alphanumeric
+         //    - if the cleaned up string is a palindrome
+         // - it reduces the ammount of back and forth between char list or arrays and strings
+         string str = s.ToLower();
+         char[] cleaInputCharArr = str.Where(c => Char.IsLetterOrDigit(c)).ToArray();
+
+         string cleanInputString = new(cleaInputCharArr);
+         Array.Reverse(cleaInputCharArr);
+         string reversedString = new(cleaInputCharArr);
+         if (cleanInputString == reversedString)
+            return true;
+
+         return false;
+      }
+
+      // This solution ( taken from LeetCode) is super fast (51 ms) and actually uses pointers
+      public bool IsPalindromeInsane(string st)
+      {
+         var s = st.ToLower();
+
+         int left = 0;
+         int right = s.Length - 1;
+
+         while (left < right)
          {
-            if (cleanInput[i] != cleanInput[cleanInputLength - 1 - i])
+            while (left < right && !Char.IsLetterOrDigit(s[left]))
+            {
+               left++;
+            }
+            while (left < right && !Char.IsLetterOrDigit(s[right]))
+            {
+               right--;
+            }
+
+            if (s[left] != s[right])
+            {
                return false;
+            }
+            left++;
+            right--;
          }
 
          return true;
       }
    }
+
+
 }
