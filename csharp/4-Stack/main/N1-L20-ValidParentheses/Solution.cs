d0081b9a3bcea004dc048431e1b41fcb7b535c2f
TaetoTum <stefanocolucci1988@gmail.com>
2022-05-09 23:57:04 +0200

    FEAT : solves L20

--- csharp/4-Stack/main/N1-L20-ValidParentheses/Solution.cs
+++ csharp/4-Stack/main/N1-L20-ValidParentheses/Solution.cs
@@ -2,9 +2,36 @@ namespace Stack.Main.ValidParentheses
 {
    public class Solution
    {
+      // time complexity : O(n) <-- string is traversed only once
+      // space complexity : O(n) <-- max size of the stack
       public bool IsValid(string s)
       {
-         return false;
+
+         if (s.Length %2 != 0)
+            return false;
+
+         Stack<char> stack = new();
+         Dictionary<char, char> closeOpenBracketsMatch = new()
+         {
+            {')','('},
+            {']','['},
+            {'}','{'}
+         };
+
+         foreach (char c in s)
+         {
+            if (closeOpenBracketsMatch.ContainsKey(c))
+            {
+               if (stack.Count != 0 && stack.Peek() == closeOpenBracketsMatch[c])
+                  stack.Pop();
+               else 
+                  return false;
+            }
+            else
+               stack.Push(c);
+         }
+
+         return stack.Count == 0 ? true : false; 
       }
    }
 
--- csharp/4-Stack/testCases/N1-L20-ValidParentheses_Test.cs
+++ csharp/4-Stack/testCases/N1-L20-ValidParentheses_Test.cs
@@ -17,4 +17,37 @@ public class ValidParentheses_Tests
 
       testResult.Should().Be(true);
    }
+
+   [Test]
+   public void Test_Example_2()
+   {
+      string input = "()[]{}";
+
+      Solution solution = new();
+      bool testResult = solution.IsValid(input);
+
+      testResult.Should().Be(true);
+   }
+
+   [Test]
+   public void Test_Example_3()
+   {
+      string input = "(]";
+
+      Solution solution = new();
+      bool testResult = solution.IsValid(input);
+
+      testResult.Should().Be(false);
+   }
+
+   [Test]
+   public void Test_Example_4()
+   {
+      string input = "{[()]}";
+
+      Solution solution = new();
+      bool testResult = solution.IsValid(input);
+
+      testResult.Should().Be(true);
+   }
 }
