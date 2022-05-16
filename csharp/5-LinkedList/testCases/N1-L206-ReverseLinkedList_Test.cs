c3f895319785ee15ff822ccd2e00886b3486e192
Stefano Colucci <stefano.colucci@ubidata.com>
2022-05-16 14:26:42 +0200

    L206 : solves iteratively and recursively

--- csharp/5-LinkedList/testCases/N1-L206-ReverseLinkedList_Test.cs
+++ csharp/5-LinkedList/testCases/N1-L206-ReverseLinkedList_Test.cs
@@ -1,3 +1,4 @@
+using FluentAssertions;
 using NUnit.Framework;
 
 namespace testCases
@@ -7,7 +8,40 @@ namespace testCases
       [Test]
       public void Test_Example_1()
       {
-         ListNode input = new();
+         ListNode input = new(1, new(2, new(3, new(4, new(5)))));
+
+         Solution solution = new();
+         ListNode testResult = solution.ReverseList(input);
+
+         testResult.val.Should().Be(5);
+         testResult.next.val.Should().Be(4);
+         testResult.next.next.val.Should().Be(3);
+         testResult.next.next.next.val.Should().Be(2);
+         testResult.next.next.next.next.val.Should().Be(1);
+
+      }
+
+      [Test]
+      public void Test_Example_2()
+      {
+         ListNode input = new(1, new(2));
+
+         Solution solution = new();
+         ListNode testResult = solution.ReverseList(input);
+
+         testResult.val.Should().Be(2);
+         testResult.next.val.Should().Be(1);
+      }
+
+      [Test]
+      public void Test_Example_3()
+      {
+         ListNode input = null;
+
+         Solution solution = new();
+         ListNode testResult = solution.ReverseList(input);
+
+         testResult.Should().BeNull();
       }
    }
 }
