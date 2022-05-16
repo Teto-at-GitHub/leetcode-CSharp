2d2e197f0c8695ee9fd222f9a1b8986fd008a246
Stefano Colucci <stefano.colucci@ubidata.com>
2022-05-16 13:38:47 +0200

    FEAT : set up L206

---
+++ csharp/5-LinkedList/testCases/N1-L206-ReverseLinkedList_Test.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace testCases
+{
+   public class ReverseLinkedList_Test
+   {
+      [Test]
+      public void Test_Example_1()
+      {
+         ListNode input = new();
+      }
+   }
+}
+
