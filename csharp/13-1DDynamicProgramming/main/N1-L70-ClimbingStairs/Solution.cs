c57bd57a1e4727a3a9aba687f6bd7cabe58d2740
Stefano Colucci <stefano.colucci@ubidata.com>
2022-05-23 13:23:34 +0200

    FEAT : adds so far wrong solution to L70

---
+++ csharp/13-1DDynamicProgramming/main/N1-L70-ClimbingStairs/Solution.cs
@@ -0,0 +1,24 @@
+namespace OneDimDynamicProgramming.Main.ClimbingStairs
+{
+   //FIXME
+   // does not work with 3
+   public class Solution
+   {
+      // tc : O(n) <== looping over n
+      // sc : O(1) <== only two variables needed, regardless of n
+      public int ClimbStairs(int n)
+      {
+         int pt1 = 1;
+         int pt2 = 1;
+
+         for (int i = 1; i < n; i++)
+         {
+            int temp = pt1;
+            pt1 = pt1 + pt2;
+            pt2 = pt1;
+         }
+
+         return pt1;
+      }
+   }
+}
