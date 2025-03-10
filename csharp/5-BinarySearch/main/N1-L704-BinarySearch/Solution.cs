223f0582bbb7e6b574a9c9a64c1bf003ff4e3647
SC <>
2025-03-10 14:56:13 +0100

    chore: moves on with refactoring

--- csharp/5-BinarySearch/main/N1-L704-BinarySearch/Solution.cs
+++ csharp/5-BinarySearch/main/N1-L704-BinarySearch/Solution.cs
@@ -2,6 +2,11 @@ namespace BinarySearch.Main.BinarySearch
 {
    public class Solution
    {
+      
+      // - time complexity: O(log n) bc if you start with n values in the array
+      // and then you don't find the target, in the next step you have n/2 possibilities to examine,
+      // then n/4 and so on. So in the worst case scenario, before you reach 1,
+      // you need to iterate as many times as you can divide n by 2, which is precisely log_2(n)  
       public int Search(int[] nums, int target)
       {
          int leftPt = 0;
