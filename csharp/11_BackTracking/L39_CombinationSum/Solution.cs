7f35e2024afafba282f04db4c79510c4f59e95fe
SC <>
2025-04-04 16:55:29 +0200

    feat: add template for backtracking problem

---
+++ csharp/11_BackTracking/L39_CombinationSum/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace _11_BackTracking.L39_CombinationSum;
+
+public class Solution
+{
+    public List<List<int>> CombinationSum(int[] nums, int target)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ csharp/_11_BackTracking_Tests/L39_CombinationSum_Tests.cs
@@ -0,0 +1,23 @@
+﻿using _11_BackTracking.L39_CombinationSum;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace _11_BackTracking_Tests;
+
+public class L39_CombinationSum_Tests
+{
+    [Test]
+    public void Example1_Test()
+    {
+        int[] numbers = new[] { 2, 5, 6, 9 };
+        int target = 9;
+
+        var solution = new Solution();
+        var result = solution.CombinationSum(numbers, target);
+
+        result.Count.Should().Be(2);
+        result[0].Should().BeEquivalentTo(new List<int>() { 2, 2, 5 });
+        result[1].Should().BeEquivalentTo(new List<int>() { 9 });
+    }
+    
+}
