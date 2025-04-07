628967b2ca578c9dfa3754af606319d5ee62fdf5
SC <>
2025-04-07 10:23:39 +0200

    feat: adds backtracking solution to L39

--- csharp/11_BackTracking/L39_CombinationSum/Solution.cs
+++ csharp/11_BackTracking/L39_CombinationSum/Solution.cs
@@ -4,6 +4,29 @@ public class Solution
 {
     public List<List<int>> CombinationSum(int[] nums, int target)
     {
-        throw new NotImplementedException();
+        var result = new List<List<int>>();
+
+        depthFirstSearch(0, new List<int>(), 0);
+
+        return result;
+
+        void depthFirstSearch(int index, List<int> current, int total)
+        {
+            if (total == target)
+            {
+                // the .ToList() is used to copy current into a new one here,
+                // otherwise it keeps getting updated
+                result.Add(current.ToList());
+                return;
+            }
+
+            if (index >= nums.Length || total > target)
+                return;
+
+            current.Add(nums[index]);
+            depthFirstSearch(index, current, total + nums[index]);
+            current.Remove(current.Last());
+            depthFirstSearch(index + 1, current, total);
+        }
     }
 }
