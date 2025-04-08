ee9f81fb3c927041513db05fa857103ad851c168
SC <>
2025-04-08 15:56:16 +0200

    feat: completes backtracking solution

--- csharp/11_BackTracking/L1863_SumOfAllSubsetsXorTotal/Solution.cs
+++ csharp/11_BackTracking/L1863_SumOfAllSubsetsXorTotal/Solution.cs
@@ -1,23 +1,26 @@
 namespace _11_BackTracking.L1863_SumOfAllSubsetsXorTotal;
 
 public class Solution {
+    
+    // Time complexity: O(2^N)
+    // Space complexity: O(N)
     public int SubsetXORSum(int[] nums)
     {
         var result = 0;
 
-        depthFirstSearch(0, 0);
+        return depthFirstSearch(0, 0);
         
-        return result;
-        
-        void depthFirstSearch(int i, int total)
+        int depthFirstSearch(int i, int total)
         {
             result += total;
             
-            if (i >= nums.Length)
-                return;
+            if (i == nums.Length)
+                return total;
 
-            depthFirstSearch(i, nums[i] ^ total);
-            depthFirstSearch(i+1, total);            
+            var sumIncludingCurrentElement = depthFirstSearch(i+1, nums[i] ^ total);
+            var sumExcludingCurrentElement = depthFirstSearch(i+1, total);
+
+            return sumIncludingCurrentElement + sumExcludingCurrentElement;
         }
     }
 
